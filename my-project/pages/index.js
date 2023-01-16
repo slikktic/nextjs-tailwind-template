8905741086504e8795d71313cd88e33d51b76e50
slikktic <49964817+slikktic@users.noreply.github.com>
2023-01-15 21:37:45 -0500

    test

--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
-          <div>
+          <div> test
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
